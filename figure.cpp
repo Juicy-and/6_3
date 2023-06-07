88f631d8d486632be303a31cc43e1f243e1b78b6
Juicy-and <131388626+Juicy-and@users.noreply.github.com>
2023-06-07 19:51:38 +0300

    Update figure.cpp

--- figure.cpp
+++ figure.cpp
@@ -2,4 +2,3 @@
 #include "WrongFigure.h"
 
 std::string Figure::get_name() { return name_; }
-//std::string WrongFigure::what() { return "Ошибка создания фигуры. Причина: сумма углов не равна 180"; }
